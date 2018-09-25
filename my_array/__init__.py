2cf6e9764db474b8d18b179d0751176c8531a54a
Maurico Espinosa <mespinosa@mespinosa-a01.vmware.com>
2018-09-25 12:47:56 -0600

    started array class

--- my_array/__init__.py
+++ my_array/__init__.py
@@ -1,2 +1,8 @@
-# - We are going to create an array 
+# init
 
+class Array:
+    pass        #-Nothing happens
+
+    def __init__(self, data):   # self is nothing 
+        self.data = data # bind data to self     - This whole section is considered the constructor (construct an object)
+        
